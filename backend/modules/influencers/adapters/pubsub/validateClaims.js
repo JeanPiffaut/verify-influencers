3628f07b5ba3842fef4464d83fa24250eb23b9ef
JeanPiffaut <piffaut.jean@gmail.com>
2025-01-08 23:38:43 -0300

    flujo completo funcional

--- backend/modules/influencers/adapters/pubsub/validateClaims.js
+++ backend/modules/influencers/adapters/pubsub/validateClaims.js
@@ -2,7 +2,12 @@ const { onMessagePublished } = require('firebase-functions/v2/pubsub');
 const { validateInfluencerClaims } = require('../../services/influencerService');
 
 exports.validateClaims = onMessagePublished('validate-claims', async (event) => {
-    const { name } = event.data.json();
+    // Decodificar el mensaje
+    const rawData = event.data.message.data; // Base64 codificado
+    const decodedData = JSON.parse(Buffer.from(rawData, 'base64').toString('utf-8'));
+
+    // Realiza el procesamiento necesario
+    const { name } = decodedData;
 
     try {
         await validateInfluencerClaims(name);
